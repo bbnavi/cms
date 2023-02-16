fa402d98cac32caf7f46b9df7b6e2436d10a803e
Janice Noack <jn@tpwd.de>
2023-02-16 14:28:03 +0100

    [TASK] adds styles for dashboard links * adds pagination arrows

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,11 @@
     {{ $t('dashboard.title') }}
   </h1>
 
-  <ul>
+  <ul class="grid grid-cols-1 gap-8 md:grid-cols-2">
     <li
       v-for="moduleName in activeModules"
       :key="moduleName"
+      class="card"
     >
       <nuxt-link :to="{ name: 'module-index', params: { module: moduleName }}">
         {{ $t(`modules.${moduleName}.title`) }}
@@ -19,3 +20,10 @@
 import { useUserStore } from '@/stores/user'
 const { activeModules } = useUserStore()
 </script>
+
+<style lang="scss">
+.card {
+  @apply flex flex-row py-10 px-6;
+  @apply bg-white rounded-lg shadow;
+}
+</style>
