a7de93d858971a2ef956b66fdc0800fa0bd5bd23
René Kersten <rk@tpwd.de>
2023-02-16 10:15:21 +0100

    feat: setup basic layout

--- components/main-nav.vue
+++ components/main-nav.vue
@@ -1,35 +1,16 @@
 <template>
-  <div class="py-4 mb-8 bg-gray-300">
-    <div class="container flex items-center justify-between mx-auto">
-      <div class="flex items-center gap-4">
+  <div class="flex-grow p-8 overflow-y-auto">
+    <ul class="space-y-3">
+      <li>
         <nuxt-link to="/">
           {{ $t('dashboard.title') }}
         </nuxt-link>
-      </div>
-
-      <div class="flex items-center justify-between gap-4">
-        <nuxt-link
-          v-if="status === 'unauthenticated'"
-          to="/login"
-        >
-          Login
-        </nuxt-link>
-
-        <div v-if="status === 'authenticated'" class="flex gap-4">
-          <div>
-            {{ data.user.email }}
-          </div>
-          <button @click="signOut">
-            Logout
-          </button>
-        </div>
-      </div>
-    </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-const { data, status, signOut } = useSession()
 </script>
 
 <style lang="scss" scoped>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <main-nav />
+  <div class="container flex min-h-screen">
+    <main-sidebar />
 
-    <div class="container mx-auto">
+    <div class="flex-grow max-h-screen p-8 overflow-y-auto">
       <slot />
     </div>
   </div>
