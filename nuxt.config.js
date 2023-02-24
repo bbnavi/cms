39adaba37ff09900b561af1d442d0b35a2303f12
René Kersten <rk@tpwd.de>
2023-02-24 17:26:55 +0100

    debug: revert back to old nuxt.config behaviour

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,67 +1,65 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig = (envVars) =>{
-  return {
-    runtimeConfig: {
-      public: {
-        authEndpoint: envVars.NUXT_PUBLIC_AUTH_ENDPOINT,
-        userEndpoint: envVars.NUXT_PUBLIC_USER_ENDPOINT,
-        authOrigin: envVars.NUXT_PUBLIC_AUTH_ORIGIN,
-        graphQlAuthEndpoint: envVars.NUXT_PUBLIC_GRAPHQL_AUTH_ENDPOINT
-      }
-    },
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      authEndpoint: process.env.NUXT_PUBLIC_AUTH_ENDPOINT,
+      userEndpoint: process.env.NUXT_PUBLIC_USER_ENDPOINT,
+      authOrigin: process.env.NUXT_PUBLIC_AUTH_ORIGIN,
+      graphQlAuthEndpoint: process.env.NUXT_PUBLIC_GRAPHQL_AUTH_ENDPOINT
+    }
+  },
 
-    css: [
-      '~/assets/css/tailwind.css',
-      '~/assets/css/main.css'
-    ],
+  css: [
+    '~/assets/css/tailwind.css',
+    '~/assets/css/main.css'
+  ],
 
-    components: {
-      "dirs": [
-        {
-          "path": "~/components/module-form-fields",
-          "global": true
-        },
-        "~/components"
-      ]
-    },
+  components: {
+    "dirs": [
+      {
+        "path": "~/components/module-form-fields",
+        "global": true
+      },
+      "~/components"
+    ]
+  },
 
-    modules: [
-      '@sidebase/nuxt-auth',
-      '@nuxtjs/apollo',
-      '@pinia/nuxt',
-      '@nuxtjs/i18n',
-      'nuxt-icons'
-    ],
+  modules: [
+    '@sidebase/nuxt-auth',
+    '@nuxtjs/apollo',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    'nuxt-icons'
+  ],
 
-    auth: {
-      origin: envVars.NUXT_PUBLIC_AUTH_ORIGIN
-    },
+  auth: {
+    origin: 'https://preview.cms.bbnavi.de'// process.env.NUXT_PUBLIC_AUTH_ORIGIN
+  },
 
-    apollo: {
-      clients: {
-        default: {
-          httpEndpoint: envVars.NUXT_PUBLIC_GRAPHQL_ENDPOINT
-        }
-      }
-    },
-
-    i18n: {
-      locales: [
-        {
-          code: 'de',
-          file: 'de-DE.json'
-        }
-      ],
-      lazy: true,
-      langDir: 'lang',
-      defaultLocale: 'de'
-    },
-
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: process.env.NUXT_PUBLIC_GRAPHQL_ENDPOINT
       }
     }
+  },
+
+  i18n: {
+    locales: [
+      {
+        code: 'de',
+        file: 'de-DE.json'
+      }
+    ],
+    lazy: true,
+    langDir: 'lang',
+    defaultLocale: 'de'
+  },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
   }
-}
+})
--- server/index.js
+++
@@ -1,4 +0,0 @@
-import getNuxtConfig from '../nuxt.config.js'
-// Import and Set Nuxt.js options
-const config = getNuxtConfig(process.env)
-const nuxt = new Nuxt(config)
