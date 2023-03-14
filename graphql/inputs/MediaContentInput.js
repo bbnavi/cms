5e4adaf418de7cf34a97e05af6a56dca81efb1b2
René Kersten <rk@tpwd.de>
2023-03-14 12:26:04 +0100

    fix: MediaContentIinput sourceUrl

--- graphql/inputs/MediaContentInput.js
+++ graphql/inputs/MediaContentInput.js
@@ -1,10 +1,14 @@
+import WebUrlInput from './WebUrlInput'
 class MediaContentInput {
   constructor(data) {
     this.captionText = data.captionText
     this.contentType = data.contentType
     this.copyright = data.copyright
     this.height = data.height
-    this.sourceUrl = data.sourceUrl // WebUrlInput
+    this.sourceUrl = new WebUrlInput({
+      url: data.sourceUrl,
+      description: data.captionText
+    })
     this.width = data.width
   }
 }
